397f58826e4cd99f3cc3abce026547d8a883e55d
DaniSua <daniferst@hotmail.com>
2023-06-06 21:15:31 -0500

    miprimercommit

---
+++ index.js
@@ -0,0 +1,8 @@
+// encuentra al elemento HTML (el boton) mediante su ID 
+const miBoton= document.getElementById ('miBoton');
+
+// Define la funcion que se ejecutara cuando se haga clic en el boton 
+function handleClick() {
+    alert ('Hola! Has hecho clic en el Boton.')
+}
+miBoton.addEventListener("click", handleClick());
